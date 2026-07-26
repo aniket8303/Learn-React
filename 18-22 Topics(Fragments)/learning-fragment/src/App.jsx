c0872f0d98758a08ef657eedf2f74c93d9edcb12
Aniket Parekar <you@example.com>
2026-07-26 09:08:13 +0530

    learn the userState hook lesson and state vs props

--- 18-22 Topics(Fragments)/learning-fragment/src/App.jsx
+++ 18-22 Topics(Fragments)/learning-fragment/src/App.jsx
@@ -3,6 +3,7 @@ import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   // let foodItems = [];
@@ -13,14 +14,25 @@ function App() {
   // let emptyMessage =
   //   foodItems.length === 0 ? <h3>I am still hungry.</h3> : null;
 
-  let foodItems = ["Sabji", "Green Vegetable", "Roti", "Salad", "Milk", "Ghee"];
+  let [foodItems, setFoodItems] = useState([]);
+  // console.log(`Current value of textstate:${textToShow}`);
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+    }
+  };
   return (
     <>
       <Container>
         <h1 className="food-heading">Healthy Foods</h1>
         {/* {emptyMessage} */}
+
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+
         <ErrorMessage items={foodItems}></ErrorMessage>
-        <FoodInput></FoodInput>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
 
--- 18-22 Topics(Fragments)/learning-fragment/src/components/FoodItems.jsx
+++ 18-22 Topics(Fragments)/learning-fragment/src/components/FoodItems.jsx
@@ -1,10 +1,22 @@
 import Item from "./Item";
+import { useState } from "react";
 
 function FoodItems({ items }) {
+  let [activeItems, setActiveItems] = useState([]);
+
+  let onBuyButton = (item, event) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <Item key={item} foodItem={item}></Item>
+        <Item
+          key={item}
+          foodItem={item}
+          bought={activeItems.includes(item)}
+          handleBuyButton={(event) => onBuyButton(item, event)}
+        ></Item>
       ))}
     </ul>
   );
